dce0387d057c8847bc0a56e6345b7b0aa816c1e6
Achemic-IDN <4chemic@gmail.com>
2025-12-15 12:04:48 +0700

    page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>PANORAMA</h1>
+      <p>Pelacakan Antrian Obat Real-time Mandiri</p>
+    </main>
+  );
+}
